0e88c0f6b5656cbac9247bf70664bfd577667f0f
Matti Niemenmaa <matti.niemenmaa+hg@iki.fi>
2008-08-19 19:32:24 +0300

    1.0.13.

--- instructions.d
+++ instructions.d
@@ -28,7 +28,7 @@ import ccbi.mini.vars         : miniMode, Mini, warnings, inMini;
 
 const cell
 	HANDPRINT      = HexCode!("CCBI"),
-	VERSION_NUMBER = 1012; // remember to change ccbi.ccbi.VERSION_STRING too!
+	VERSION_NUMBER = 1013; // remember to change ccbi.ccbi.VERSION_STRING too!
 
 int returnVal;
 char[][] fungeArgs;
