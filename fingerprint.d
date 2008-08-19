409c7b075393e3e7e68cc1c0a048da89c04ae002
Matti Niemenmaa <matti.niemenmaa+hg@iki.fi>
2008-08-19 19:07:34 +0300

    Add 3DSP fingerprint.

--- fingerprint.d
+++ fingerprint.d
@@ -17,7 +17,7 @@ void function()  [cell] fingerprintDestructors;
 // so that the destructor is called only when the loaded count is zero
 uint             [cell] fingerprintLoaded;
 
-template Code(char[4] s, char[4] id = s) {
+template Code(char[4] s, char[] id = s) {
 	const Code =
 		"const cell " ~ id ~ " = " ~ ToUtf8!(HexCode!(s)) ~ ";" ~
 		// this really shouldn't be here, but oh well
