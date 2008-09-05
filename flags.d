6f2347248b2de2866699c067a25e9b5d22fdd0e5
Matti Niemenmaa <matti.niemenmaa+hg@iki.fi>
2008-09-05 22:03:52 +0300

    Remove inane BitArray messing about, use a Flags struct instead.

---
+++ flags.d
@@ -0,0 +1,12 @@
+// File created: 2008-09-05 21:48:48
+
+module ccbi.flags;
+
+struct Flags {
+	bool
+		useStats            = false,
+		script              = false,
+		tracing             = false,
+		fingerprintsEnabled = true,
+		warnings            = false;
+}
