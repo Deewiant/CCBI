a71523a3add89f572dc7a434f052bdc6dc869e93
Matti Niemenmaa <matti.niemenmaa+hg@iki.fi>
2008-09-13 23:29:00 +0300

    1.0.15.

--- fingerprints/rcfunge98/indv.d
+++ fingerprints/rcfunge98/indv.d
@@ -22,6 +22,18 @@ static this() {
 	fingerprints[INDV]['W'] =& putVec;
 }
 
+// get vector off of stack,,,,apply storage offset,,,,retrieve vector from
+// funge-space,,,,apply storage offset,,,,retrieve vector from funge-space to
+// stack,,,,,do not modify this last read vector...
+
+// in all funcitons of INDV,,,,a vector is popped off the stack and the storage
+// offset is applied,,,,that points to a vector in memory which is retrieved
+// and the storage offset applied,,,,whatever data that points to is read as
+// is....
+
+// the final data read or written is not modified by the storage offset...
+// only the 2 pointer vectors are... 
+
 void getNum() {
 	cellidx x, y;
 	popVector(x, y);
