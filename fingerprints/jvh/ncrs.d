7c7d02805762c596507745bbbe7cf431f52e7c44
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-04-25 18:47:41 +0300

    NCRS.

--- fingerprints/jvh/ncrs.d
+++ fingerprints/jvh/ncrs.d
@@ -2,12 +2,9 @@
 
 // File created: 2007-01-20 21:16:38
 
-module ccbi.fingerprints.jvh.ncrs; private:
+module ccbi.fingerprints.jvh.ncrs;
 
 import ccbi.fingerprint;
-import ccbi.instructions : reverse;
-import ccbi.ip;
-import ccbi.utils;
 
 version (Windows)
 	version = PDCurses;
@@ -20,7 +17,7 @@ version (PDCurses) {
 	pragma (msg, "Remember to link with an ncurses library.");
 }
 
-pragma (msg, "Assuming 32-bit chtype... correct ccbi.fingerprints.jvh.ncrs.chtype to ushort if link errors ensue.");
+pragma (msg, "Assuming 32-bit chtype...");
 alias uint chtype;
 
 extern (C) {
@@ -65,31 +62,33 @@ extern (C) {
 // Ncurses [sic] extension
 // -----------------------
 
-static this() {
-	mixin (Code!("NCRS"));
+mixin (Fingerprint!(
+	"NCRS",
 
-	fingerprints[NCRS]['B'] =& doBeep;
-	fingerprints[NCRS]['C'] =& clear;
-	fingerprints[NCRS]['E'] =& toggleEcho;
-	fingerprints[NCRS]['G'] =& get;
-	fingerprints[NCRS]['I'] =& init;
-	fingerprints[NCRS]['K'] =& toggleKeypad;
-	fingerprints[NCRS]['M'] =& gotoxy;
-	fingerprints[NCRS]['N'] =& toggleInput;
-	fingerprints[NCRS]['P'] =& put;
-	fingerprints[NCRS]['R'] =& refresh;
-	fingerprints[NCRS]['S'] =& write;
-	fingerprints[NCRS]['U'] =& unget;
-}
+	"B", "doBeep",
+	"C", "clear",
+	"E", "toggleEcho",
+	"G", "get",
+	"I", "init",
+	"K", "toggleKeypad",
+	"M", "gotoxy",
+	"N", "toggleInput",
+	"P", "put",
+	"R", "refresh",
+	"S", "write",
+	"U", "unget"
+));
 
 enum { ERR = -1 }
 
-void doBeep () { if (beep()                == ERR) reverse(); }
-void refresh() { if (wrefresh(stdscr)      == ERR) reverse(); }
-void unget  () { if (ungetch(ip.stack.pop) == ERR) reverse(); }
+template NCRS() {
+
+void doBeep () { if (beep()                 == ERR) reverse; }
+void refresh() { if (wrefresh(stdscr)       == ERR) reverse; }
+void unget  () { if (ungetch(cip.stack.pop) == ERR) reverse; }
 
 void clear() {
-	switch (ip.stack.pop) {
+	switch (cip.stack.pop) {
 		case 0: return werase(stdscr);
 		case 1: return wclrtoeol(stdscr);
 		case 2: return wclrtobot(stdscr);
@@ -97,14 +96,14 @@ void clear() {
 	}
 }
 
-void toggleEcho  () { if ((ip.stack.pop ? echo    () : noecho()) == ERR) reverse(); }
-void toggleInput () { if ((ip.stack.pop ? nocbreak() : cbreak()) == ERR) reverse(); }
-void toggleKeypad() { if (keypad(stdscr, cast(bool)ip.stack.pop) == ERR) reverse(); }
+void toggleEcho  () { if ((cip.stack.pop ? echo    () : noecho()) == ERR) reverse; }
+void toggleInput () { if ((cip.stack.pop ? nocbreak() : cbreak()) == ERR) reverse; }
+void toggleKeypad() { if (keypad(stdscr, cast(bool)cip.stack.pop) == ERR) reverse; }
 
-void get() { ip.stack.push(cast(cell)wgetch(stdscr)); }
+void get() { cip.stack.push(cast(cell)wgetch(stdscr)); }
 
 void init() {
-	if (ip.stack.pop) {
+	if (cip.stack.pop) {
 		if (initscr() is null)
 			reverse();
 	} else {
@@ -114,11 +113,12 @@ void init() {
 }
 
 void gotoxy() {
-	cellidx x, y;
-	popVector(x, y);
-	if (wmove(stdscr, y, x) == ERR)
+	cell y = cip.stack.pop;
+	if (wmove(stdscr, y, cip.stack.pop) == ERR)
 		reverse();
 }
 
-void put()   { if (waddch (stdscr, cast(chtype)ip.stack.pop) == ERR) reverse(); }
-void write() { if (waddstr(stdscr, cast(char*) popStringz()) == ERR) reverse(); }
+void put()   { if (waddch (stdscr, cast(chtype)cip.stack.pop) == ERR) reverse; }
+void write() { if (waddstr(stdscr, popStringz())              == ERR) reverse; }
+
+}
