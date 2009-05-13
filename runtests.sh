a19c45223c703fa1b1e5d41393c8058d3a65506b
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-05-13 18:09:24 +0300

    Add space tests, make runtests get input for them.

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
+
+n=`grep 'NEWBOX_PAD[[:space:]]*=' src/ccbi/space.d | sed -E 's/.*([0-9]+);/\1/'`
+if [ -z $n ]; then
+	echo "NEWBOX_PAD not found in src/ccbi/space.d!"
+	exit 1
+fi
+ins=
+for f in tests/space/*.t; do
+	if [ ! -e "$f.in" ]; then
+		echo $n > "$f.in"
+		ins="$ins $f.in"
+	fi
+done
+
 prove -e tests/runner.pl -r tests $*
+
+rm $ins
---
+++ tests/space/fusion-simple.b98.t
@@ -0,0 +1,4 @@
+v File created: 2009-05-09 21:55:51
+  Places two blocks in the order 12: should get fused without data loss
+> a"2..1"4k,& 'a0fp :'b\:+1+fp 0fg'a-#v_        >a"1 ko"4k,  ::+1+fg'b-#v_        >a"2 ko"4k,@
+                                      >" ton"3k,^                       >" ton"3k,^
