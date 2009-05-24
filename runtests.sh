bd4941c3ac0b8c294fde913244f54731655a0742
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-05-24 14:46:54 +0300

    Make disjoint tests actually subsume disjoints.
    Required some fiddling with cheaperToAlloc as well as the tests
    themselves. The current alternatives for disjoint.t98.t are one box of
    10115 cells or two boxes of 4913 cells each, so ACCEPTABLE_WASTE had to
    be at least 279: 4*(10115-x) < 4*4913*2 + 40 when x > 279. Previously,
    the difference required would've been even greater.

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-n=`grep 'NEWBOX_PAD[[:space:]]*=' src/ccbi/space.d | sed -E 's/.*([0-9]+);/\1/'`
+n=`grep 'NEWBOX_PAD[[:space:]]*=' src/ccbi/space.d | sed -E 's/.*([0-9]+)[,;]/\1/'`
 if [ -z $n ]; then
 	echo "NEWBOX_PAD not found in src/ccbi/space.d!"
 	exit 1
