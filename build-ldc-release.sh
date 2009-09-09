61194cd81e76bebe5986d475a291f5ec065c2086
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-09-09 11:43:03 +0300

    Don't link in *template*.o in the build-ldc-*.sh.

--- build-ldc-release.sh
+++ build-ldc-release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ldc -singleobj -oq -Isrc -L-lncurses -O5 -release $* -of=bin/ccbi -od=obj src/ccbi/ccbi.d `ldmd -v -Isrc -o- -oq src/ccbi/ccbi.d | grep import | grep -vE 'tango.core|tango.ldc|di\)' | perl -pe 's/import\s+[^\s]+\s+\((.+)\)/\1/'`
+ldc -singleobj -oq -Isrc -L-lncurses -O5 -release $* -of=bin/ccbi -od=obj src/ccbi/ccbi.d `ldmd -v -Isrc -o- -oq src/ccbi/ccbi.d | grep import | grep -vE 'tango.core|tango.ldc|di\)' | perl -pe 's/import\s+[^\s]+\s+\((.+)\)/\1/' | fgrep -v template`
